31086f12c6708bae2ee17f40f807769257b5e79f
65LimesAH <52928425+65LimesAH@users.noreply.github.com>
2020-11-09 14:08:49 -0600

    init

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import { Tedis, TedisPool } from 'tedis';
+
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Account: {
+    _resolveReference(object) {
+      return accounts.find((account) => account.id === object.id);
+    },
+  },
+
+  Query: {
+    hello: () => 'Hello world!',
+  },
+  Mutation: {
+    login(parent, { email, password }) {
+      const { id, permissions, roles } = accounts.find(
+        (account) => account.email === email && account.password === password,
+      );
+      return jwt.sign(
+        { 'https://awesomeapi.com/graphql': { roles, permissions } },
+        'f1BtnWgD3VKY',
+        { algorithm: 'HS256', subject: id, expiresIn: '1d' },
+      );
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
